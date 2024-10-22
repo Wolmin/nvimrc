72b4136e0104499c93ba9ffe25ed5c882b65a743
Wolmin <lamonos123@gmail.com>
2024-10-22 10:50:43 +0200

    Add toggleterm

--- lua/wolmin/remap.lua
+++ lua/wolmin/remap.lua
@@ -2,3 +2,7 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 vim.keymap.set("v", "<A-Up>", ":m '<-2<CR>gv=gv")
 vim.keymap.set("v", "<A-Down>", ":m '>+1<CR>gv=gv")
+
+vim.keymap.set({"n", "v"}, "<leader>hh", ":ToggleTerm direction=horizontal<CR>")
+vim.keymap.set({"n", "v"}, "<leader>v", ":ToggleTerm direction=vertical<CR>")
+vim.keymap.set({"n", "v"}, "<leader>hf", ":ToggleTerm direction=float<CR>")
