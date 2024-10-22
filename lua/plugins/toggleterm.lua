36ffef5cd4881cae1b340d610c47502610f1dca0
Wolmin <lamonos123@gmail.com>
2024-10-22 12:47:21 +0200

    Move bindings for terminal

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,3 +1,12 @@
 return {
-  {'akinsho/toggleterm.nvim', version = "*", config = true}
+  {
+    'akinsho/toggleterm.nvim',
+    version = "*",
+    config = function()
+      require('toggleterm').setup{}
+      vim.keymap.set({"n", "v"}, "<leader>hh", ":ToggleTerm direction=horizontal<CR>")
+      vim.keymap.set({"n", "v"}, "<leader>v", ":ToggleTerm direction=vertical<CR>")
+      vim.keymap.set({"n", "v"}, "<leader>hf", ":ToggleTerm direction=float<CR>")
+    end
+  }
 }
--- lua/wolmin/remap.lua
+++ lua/wolmin/remap.lua
@@ -4,6 +4,8 @@ vim.keymap.set("v", "<A-Up>", ":m '<-2<CR>gv=gv")
 vim.keymap.set("v", "<A-Down>", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "<C-c>", "")
 
+vim.keymap.set({"n", "v"}, "<leader>hh", function() require('toggleterm.terminal').Terminal:new({direction = "horizontal"}):toggle() end)
+
 vim.keymap.set({"n", "v"}, "<leader>hh", ":ToggleTerm direction=horizontal<CR>")
 vim.keymap.set({"n", "v"}, "<leader>v", ":ToggleTerm direction=vertical<CR>")
 vim.keymap.set({"n", "v"}, "<leader>hf", ":ToggleTerm direction=float<CR>")
