93572fd7dc8e0536d3f4fd2eb481465bd14c0be9
Wolmin <44748271+Wolmin@users.noreply.github.com>
2024-10-24 00:23:14 +0200

    Enable transparency

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,1 +1,16 @@
-vim.cmd.colorscheme "base16-onedark"
+function Colors(color)
+  color = color or "base16-onedark"
+  vim.cmd.colorscheme(color)
+
+  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+  vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })     -- Non-current windows
+  vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })   -- Sign column
+  vim.api.nvim_set_hl(0, "StatusLine", { bg = "none" })   -- Status line
+  vim.api.nvim_set_hl(0, "StatusLineNC", { bg = "none" }) -- Inactive status line
+  vim.api.nvim_set_hl(0, "VertSplit", { bg = "none" })    -- Vertical split
+  vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })  -- End of buffer characters
+  vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })       -- Line numbers
+end
+
+Colors()
